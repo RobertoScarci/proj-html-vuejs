8c78a8fb67fc04543d814dc389c73530f0fbf399
RobertoScarci <robertoscarci.02@gmail.com>
2024-01-08 14:21:03 +0100

    aggiunta del jumbo all'interno della sezione header.vue

--- proj-html-vuejs/src/components/AppHeader.vue
+++ proj-html-vuejs/src/components/AppHeader.vue
@@ -2,10 +2,10 @@
     <header class="pt-3 pb-2">
         <div class="my_container">
             <div class="row">
-                <div class="col-2">
+                <div class="col-4">
                    <img src="../assets/logo-dark.png" alt="Land's logo">
                 </div>
-            <div class="col-5">
+            <div class="col-4">
                 <ul>
                     <li>HOME</li>
                     <li>LANDING</li>
@@ -13,8 +13,32 @@
                     <li>DOCS</li>
                 </ul>
             </div>
-            <div class="settings col-5">
+            <div class="settings col-4">
                 Search
+            </div>
+            </div>
+        </div>
+        <div class="my_container jumbo">
+            <div class="row">
+                <div class="col-6">
+                <p>
+                    Develoment
+                </p>
+                <h3>
+                    A Complete Developer Toolset
+                </h3>
+                <p class="p-3">
+                    Launch your campaign and benefit from our expertise on designing and managing conversion centered bootstrap4 html page.
+                </p>
+                <button class="m-2">
+                    Get Started
+                </button>
+                <button>
+                    Documentation
+                </button>
+            </div>
+            <div class="col-6">
+                <img src="../assets/about.png" alt="about's img">
             </div>
             </div>
         </div>
@@ -28,16 +52,12 @@
 @use "../styles/general.scss" as *;
 header{
     background-color: #F8F9FC;
-}
-
-.my_container{
-    width: 100%;
+    text-align: center;
 }
 
 ul{
     list-style-type: none;
     display: flex;
-    padding: 0 2rem;
     font-size: .9rem;
 }
 
@@ -46,13 +66,27 @@ li{
 }
 
 img{
-    width: calc(100% / 2);
+    width: calc(100% / 4);
+    text-align: center;
+    vertical-align: top;
+}
+
+.settings{
+    padding-right: 0;
+    justify-content: center;
+}
+
+.jumbo{
+    width: 80%;
+    padding-top: 8rem;
+}
+
+.jumbo .row{
     display: flex;
     justify-content: center;
 }
 
-.settings{
-    padding-left: 3rem;
-    justify-content: center;
+.jumbo img{
+    width: 80%;
 }
 </style>
