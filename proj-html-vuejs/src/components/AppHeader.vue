3927f404a6bd4305e7eed8c645791a413809436a
RobertoScarci <robertoscarci.02@gmail.com>
2024-01-09 11:39:11 +0100

    completamento della sezione header

--- proj-html-vuejs/src/components/AppHeader.vue
+++ proj-html-vuejs/src/components/AppHeader.vue
@@ -1,11 +1,11 @@
 <template>
     <header class="pt-3 pb-2">
-        <div class="my_container">
+        <div class="container">
             <div class="row">
                 <div class="col-4">
                    <img src="../assets/logo-dark.png" alt="Land's logo">
                 </div>
-            <div class="col-4">
+            <div class="col-4 pr-2">
                 <ul>
                     <li>HOME</li>
                     <li>LANDING</li>
@@ -18,28 +18,58 @@
             </div>
             </div>
         </div>
-        <div class="my_container jumbo">
+        <div class="container jumbo">
             <div class="row">
-                <div class="col-6">
-                <p>
+                <div class="col-6 pt-5">
+                <p class="pb-3">
                     Develoment
                 </p>
-                <h3>
+                <h1 class="">
                     A Complete Developer Toolset
-                </h3>
-                <p class="p-3">
+                </h1>
+                <p class="pt-3 pb-3">
                     Launch your campaign and benefit from our expertise on designing and managing conversion centered bootstrap4 html page.
                 </p>
-                <button class="m-2">
+                <button class="btn btn-primary">
                     Get Started
                 </button>
-                <button>
+                <button class="btn btn-outline-primary">
                     Documentation
                 </button>
+                <p class="pt-3">
+                    Current Version: v2.6.0
+                </p>
+                </div>
+                <div class="col-6">
+                    <img src="../assets/about.png" alt="about's img">
+                </div>
             </div>
-            <div class="col-6">
-                <img src="../assets/about.png" alt="about's img">
-            </div>
+        </div>
+        <hr>
+        <div class="container logos">
+            <div class="row">
+                <div class="col-12">
+                    <ul>
+                        <li>
+                            <img src="../assets/amazon.svg" alt="Amazon logo">
+                        </li>
+                        <li>
+                            <img src="../assets/google.svg" alt="Amazon logo">
+                        </li>
+                        <li>
+                            <img src="../assets/lenovo.svg" alt="Amazon logo">
+                        </li>
+                        <li>
+                            <img src="../assets/paypal.svg" alt="Amazon logo">
+                        </li>
+                        <li>
+                            <img src="../assets/shopify.svg" alt="Amazon logo">
+                        </li>
+                        <li>
+                            <img src="../assets/spotify.svg" alt="Amazon logo">
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     </header>
@@ -49,7 +79,11 @@
 </script>
 
 <style lang="scss" scoped>
+
 @use "../styles/general.scss" as *;
+@use "../styles/partials/mixins" as *;
+@use "../styles/partials/variables" as *;
+
 header{
     background-color: #F8F9FC;
     text-align: center;
@@ -61,7 +95,7 @@ ul{
     font-size: .9rem;
 }
 
-li{
+.col-4 li{
     margin-left: 3.5rem;
 }
 
@@ -77,8 +111,10 @@ img{
 }
 
 .jumbo{
-    width: 80%;
+    width: 70%;
     padding-top: 8rem;
+    padding-bottom: 5rem;
+    text-align: left;
 }
 
 .jumbo .row{
@@ -87,6 +123,31 @@ img{
 }
 
 .jumbo img{
+    width: 90%;
+}
+
+.btn-outline-primary{
+    margin-left: .8rem;
+}
+
+.col-12 ul{
+    padding-top: 2rem;
+    width: 100%;
+}
+
+.logos{
     width: 80%;
 }
+
+p{
+    color: $text-color;
+    font-size: .8rem;
+}
+
+h1{
+    font-size: 2.5rem;
+    font-weight: bold;
+    line-height: 1.5;
+}
+
 </style>
